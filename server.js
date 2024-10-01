3d7a2467ab0e89f576b0259c51a93af3b6df0d4e
luan048 <luanmonteiropedrosa@gmail.com>
2024-09-30 23:10:37 -0300

    Create Server

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
-// It's waiting the project
-// Make the other middleware 
+import express from "express";
+
+import {routerAppointment} from './routes/routesAppointment'
+import {routerAuth} from './routes/routesAuth'
+import {routerDentist} from './routes/routesDentist'
+
+const server = express()
+const port = 8001
+
+server.use(express.json())
+server.use(routerAppointment)
+server.use(routerAuth)
+server.use(routerDentist)
+
+server.listen(port, () => {
+    tryToConnect()
+    console.log(`Running in port: ${port}`)
+})
